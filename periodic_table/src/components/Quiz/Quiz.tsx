21f2a98f5db24494f508ae922ca58c749a9dec12
1luckycat <027.mott@gmail.com>
2024-03-06 22:37:17 -0600

    added some css to quiz page

--- periodic_table/src/components/Quiz/Quiz.tsx
+++ periodic_table/src/components/Quiz/Quiz.tsx
@@ -1,10 +1,18 @@
 import * as _React from 'react';
+import './quiz.css';
+import { NavBar } from '../sharedComponents';
 
 
 export const Quiz = () => {
     return (
         <div>
-            <h1 style={{ marginTop: '50px'}}>This is the Quiz Page</h1>
+            <NavBar />
+            <h1 className="quizPage"style={{ marginTop: '50px'}}>A
+            <span className='quiz' id='q'>Q</span>
+            <span className='quiz' id='u'>u</span>
+            <span className='quiz' id='i'>i</span>
+            <span className='quiz' id='z'>z</span>Page</h1>
         </div>
+
     )
 }
