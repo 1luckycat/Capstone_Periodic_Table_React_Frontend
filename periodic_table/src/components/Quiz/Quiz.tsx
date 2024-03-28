1a7fdc6b6793d3392ee925889513eef4f6d76da2
1luckycat <027.mott@gmail.com>
2024-03-28 16:16:45 -0500

    got elements in their own flashcard

--- periodic_table/src/components/Quiz/Quiz.tsx
+++ periodic_table/src/components/Quiz/Quiz.tsx
@@ -2,32 +2,72 @@ import * as _React from 'react';
 import './quiz.css';
 import { NavBar } from '../sharedComponents';
 import { useGetElementTable } from '../../customHooks';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { serverCalls } from '../../api';
 
 
 export const Quiz = () => {
 
     const { elementTableData } = useGetElementTable();
     const [ flip, setFlip ] = useState(false)
+    // const [ flashcards, setFlashcards ] = useState(elementTableData)
+    const [ loading, setLoading ] = useState(true)
 
     console.log(elementTableData)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                
+                setLoading(true);
+                
+                const data = await serverCalls.getElementTable();
+
+                if (data) {
+                    setLoading(false);
+                }
+                
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false); 
+            }
+        };
+
+        fetchData(); 
+    }, []);
+
 
     return (
         <div>
             <NavBar />
-            <div className={`card ${flip ? 'flip' : ''}`}
+            <div className={`flashcard-container ${flip ? 'flip' : ''}`}
             onClick={() => setFlip(!flip)}
             >
-                <div className='front'>
+                {elementTableData.map((flashcard, index) => (
+                    <div className='flashcard' key={index}>
+                        <div className='front'>
+                            <div className='flashcard-symbol'>{flashcard.symbol}</div>
+                        </div>
+                        <div className='back'>
+                            <div className='flashcards-name'>{flashcard.name}</div>
+                        </div>
+                    </div>
+                ))}
+
+
+                {/* THIS WORKS BEST BUT STILL FLIPS ALL ELEMENTS */}
+                {/* <div className='front'>
                     
-                    { elementTableData.map((flashcard) => {
-                        return <div className = 'flashcard-symbol'>{flashcard.symbol}</div>
+                    { elementTableData.map((flashcards) => {
+                        return <div className = 'flashcards-symbol'>{flashcards.symbol}</div>
                     })}
                 </div>
                 <div className='back'>
-                    { elementTableData.map((flashcard) => {
-                        return <div className = 'flashcard-name'>{flashcard.name}</div>
-                    })}</div>
+                    { elementTableData.map((flashcards) => {
+                        return <div className = 'flashcards-name'>{flashcards.name}</div>
+                    })}</div> */}
+
+
 
             {/*  THIS WORKS WITH FLIP, BUT FLIPS ALL ELEMENTS */}
             {/* { elementTableData.map((flashcard) => (
